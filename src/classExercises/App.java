2a425999424c5a838c117a7901f9174003503f20
andyking <andrewk640@gmail.com>
2024-01-16 10:55:50 +0000

    1st day of oop, used imports added getters and setters with validation

--- src/classExercises/App.java
+++ src/classExercises/App.java
@@ -1,25 +1,30 @@
 package classExercises;
 
+import oop.Person;
+
 public class App {
 
 	public static void main(String[] args) {
 
 		Person me = new Person();
-		me.name = "Andrew";
-		me.age = 27;
-		me.location = "Bradford";
+		me.setName("");
+		me.setAge(27);
+		me.setLocation("Bradford");
 
 		me.printPerson();
 		me.intro();
 
 		Person myCat = new Person();
-		myCat.name = "Ember";
-		myCat.age = 6;
-		myCat.location = "Cat City";
+		myCat.setName("Ember");
+		myCat.setAge(6);
+		myCat.setLocation("Cat City");
 
 		myCat.printPerson();
 
 		myCat.intro();
 
+		me.setAge(me.getAge() + 100);
+
 	}
+
 }
---
+++ src/oop/Person.java
@@ -0,0 +1,56 @@
+package oop;
+
+public class Person {
+
+	private String name;
+
+	private int age;
+
+	private String location;
+
+	public void intro() {
+		System.out.println("Hello my name is " + name + ". I am " + age + " years old" + " and I am from " + location);
+	}
+
+	public void printPerson() {
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(location);
+	}
+
+	public void setAge(int age) {
+		if (age <= 100 && age >= 1) {
+			this.age = age;
+		} else {
+			System.out.println("Please enter an age between 1 and 100");
+		}
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setName(String name) {
+		if (name.isBlank()) {
+			System.out.println("Please enter a valid name");
+		} else {
+			this.name = name;
+		}
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setLocation(String location) {
+		if (location.isBlank()) {
+			System.out.println("Please enter a valid location");
+		} else {
+			this.location = location;
+		}
+	}
+
+	public String getLocation() {
+		return this.location;
+	}
+}
